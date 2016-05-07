0b675f77464901c020b26f3fe53a3d77a637373f
Simon van der Meer <simon_vd_meer@live.nl>
2016-05-07 19:11:19 +0200

    Little bit of restructuring

--- src/cisim/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CIRCUITSIMULATOR_MAIN_H_
-#define _CIRCUITSIMULATOR_MAIN_H_
-
-#include "circuitsimulator.h"
-
-#endif // _CIRCUITSIMULATOR_MAIN_H_
---
+++ src/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include "cisim/circuitsimulator.h"
+
+#endif // _MAIN_H_
